3344d3441926a8a77cccabc330aab68bd75afd4e
mattgoldman93 <mattgoldman93@gmail.com>
2017-10-11 15:09:01 -0400

    Add user auth form and upload form
    Pair Programmed: Kat and Matt
    Adds auth form in index.html and adds events, api, ui javascript files
    to allow users to sign up, sign in successfully. This is a skeleton so
    some ui code is commented out for now.
    Add upload form for users to upload a file. And adds api, events, ui
    javascript files to connect to the api.

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,53 @@
+'use strict'
+const store = require('../store')
+// const logic = require('../game/logic')
+
+const signUpSuccess = function (data) {
+  $('#message').show()
+  $('#message').text('Successfully signed up, go ahead and sign in!')
+  $('#message').fadeOut(1800)
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
+}
+
+const signInSuccess = function (data) {
+  $('#message').show()
+  $('#message').text('Successfully signed in!')
+  // $('.initial-hide').show()
+  // $('.secondary-hide').hide()
+  // $('#message').fadeOut(1800)
+  store.user = data.user
+  // $('input s[type=text]').val('')
+  // $('input[type=password]').val('')
+}
+
+const changePasswordSuccess = function (data) {
+  $('#message').show()
+  $('#message').text('Password changed!')
+  $('#message').fadeOut(1800)
+  $('input[type=password]').val('')
+}
+
+const signOutSuccess = function (data) {
+  $('#message').show()
+  $('#message').text('Signed out!')
+  $('.initial-hide').hide()
+  $('.secondary-hide').show()
+  $('#message').fadeOut(1800)
+  store.user = null
+}
+
+const failure = function (error) {
+  console.error(error)
+  // $('#message').show()
+  // $('#message').text('You messed up')
+  // $('#message').fadeOut(1800)
+}
+
+module.exports = {
+  signUpSuccess,
+  signInSuccess,
+  changePasswordSuccess,
+  signOutSuccess,
+  failure
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,11 +2,16 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const authEvents = require('./auth/events.js')
+const uploadEvents = require('./uploads/events')
+
 
 $(() => {
   setAPIOrigin(location, config)
+  authEvents.addHandlers()
+  $('.initial-hide').hide()
+    $('#multipart-form-data').on('submit', uploadEvents.createUploadMultiPart)
 })
-
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
