b7ba838faf51a3bdd8c8addaaedd94f571b6ba6d
A-Aron <vale.aaron@gmail.com>
2017-10-13 15:47:09 -0400

    Fix Bug with Edit
    Fixes bug where the wrong column was editable.
    Removes 'Success getting files' message.
    Changes the message fadeout time from 5 to 3 seconds.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -16,27 +16,27 @@ const signUpSuccess = function (data) {
   $('#message').show()
   $('#message').text('Successfully signed up, go ahead and sign in!')
   successSound.play()
-  $('#message').fadeOut(5000)
+  $('#message').fadeOut(3000)
   resetFormFields()
 }
 
 const signUpFailure = function () {
   $('#message').show()
-  $('#message').text('Failure on signing up. E-mail and/or password already taken. Try again.').fadeOut(5000)
+  $('#message').text('Failure on signing up. E-mail and/or password already taken. Try again.').fadeOut(3000)
   failureSound.play()
   resetFormFields()
 }
 
 const signInSuccess = function (data) {
   $('#message').show()
-  $('#message').text('Successfully signed in!').fadeOut(5000)
+  $('#message').text('Successfully signed in!').fadeOut(3000)
   $('.initial-hide').show()
   $('.secondary-hide').hide()
   store.user = data.user
   $('#auth-modal').modal('hide')
   successSound.play()
   resetFormFields()
-  
+
   uploadApi.getUploads()
     .then(uploadUi.onGetUploadsSuccess)
     .catch(uploadUi.onGetUploadsFailure)
@@ -44,7 +44,7 @@ const signInSuccess = function (data) {
 
 const signInFailure = function () {
   $('#message').show()
-  $('#message').text('Failure on signing in. Try again.').fadeOut(5000)
+  $('#message').text('Failure on signing in. Try again.').fadeOut(3000)
   failureSound.play()
   resetFormFields()
 }
@@ -53,13 +53,13 @@ const changePasswordSuccess = function (data) {
   $('#message').show()
   $('#message').text('Password changed!')
   successSound.play()
-  $('#message').fadeOut(5000)
+  $('#message').fadeOut(3000)
   resetFormFields()
 }
 
 const changePasswordFailure = function () {
   $('#message').show()
-  $('#message').text('Failure on changing password. Try again.').fadeOut(5000)
+  $('#message').text('Failure on changing password. Try again.').fadeOut(3000)
   resetFormFields()
   failureSound.play()
 }
@@ -69,7 +69,7 @@ const signOutSuccess = function (data) {
   $('#message').text('Signed out!')
   $('.initial-hide').hide()
   $('.secondary-hide').show()
-  $('#message').fadeOut(5000)
+  $('#message').fadeOut(3000)
   $('#auth-modal').modal('hide')
   store.user = null
   $('.uploads-table').html('')
@@ -79,7 +79,7 @@ const signOutSuccess = function (data) {
 const signOutFailure = function () {
   $('#message').show()
   $('#message').text('Failure on signing out. Try Again.')
-  $('#message').fadeOut(5000)
+  $('#message').fadeOut(3000)
   resetFormFields()
   failureSound.play()
 }
