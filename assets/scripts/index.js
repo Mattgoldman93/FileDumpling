6e6892121837436ad0f2f5c89f104559d55db7d2
mattgoldman93 <mattgoldman93@gmail.com>
2017-10-13 10:17:25 -0400

    Add styling
    adds style to document:
      - change background color to light grey
      - text is now green
      - file upload form centered
      - refresh button moved to top of table/ right
    Moves elements around. File input now uses
    function to display file name in separate input field.
    Refresh button functionality still needs to be tested.
    
    Programmed as an entire group -- Matt, Kat, Aaron, Vitorio

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -29,6 +29,24 @@ $(() => {
   $('.initial-hide').hide()
   $('#multipart-form-data').on('submit', uploadEvents.createUploadMultiPart)
 })
+
+$(function () {
+  $(document).on('change', ':file', function () {
+    const input = $(this)
+    const numFiles = input.get(0).files ? input.get(0).files.length : 1
+    const label = input.val().replace(/\\/g, '/').replace(/.*\//, '')
+    input.trigger('fileselect', [numFiles, label])
+  })
+  $(document).ready(function () {
+    $(':file').on('fileselect', function (event, numFiles, label) {
+      const input = $(this).parents('.input-group').find(':text')
+      const log = numFiles > 1 ? numFiles + ' files selected' : label
+      if (input.length) {
+        input.val(log)
+      }
+    })
+  })
+})
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
